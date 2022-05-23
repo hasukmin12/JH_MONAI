dd4a3c7e898b45381007aaa474e9f1868fa6c2c6
hasukmin12 <hasukmin12@naver.com>
2022-05-23 04:52:28 +0000

    update

--- inference.py
+++ inference.py
@@ -22,39 +22,50 @@ from utils import *
 def main():
     parser = ap.ArgumentParser()
     parser.add_argument('--data_dir', '-i', default="/disk1/sukmin/dataset/Task302_KiPA", type=str)
-    parser.add_argument('--output_dir', '-o', default="/disk1/sukmin/inf_rst", type=str)
-    parser.add_argument('--pth_path', default="/disk1/sukmin/unet/kipa/fold4/model_best.pth", type=str)
-    parser.add_argument('--model', '-m', default='unet', dest='MODEL_NAME', type=str)
-
-    # parser.add_argument('--input', default='96,96,96', dest='input_shape', type=str)
+    parser.add_argument('--output_dir', '-o', default="/disk1/sukmin/inf_rst/kipa_model", type=str)
+    parser.add_argument('--model_path', default="/disk1/sukmin/kipa_model", type=str)
+    parser.add_argument('--model', '-m', default='unetr', dest='MODEL_NAME', type=str)
+    parser.add_argument('--save_name', default='unetr_ce', type=str)
 
     # default
-    # parser.add_argument('--window', default = '0,300',  dest='CONTRAST', type=str)# default='-150,300', dest='CONTRAST', type=str)
     parser.add_argument('--roi_x', default=96, type=int, help='roi size in x direction')
     parser.add_argument('--roi_y', default=96, type=int, help='roi size in y direction')
     parser.add_argument('--roi_z', default=96, type=int, help='roi size in z direction')
     parser.add_argument('--channel_in', default=1, dest='channel_in', type=int)
     parser.add_argument('--channel_out', default=5, dest='channel_out', type=int)
+
     parser.add_argument('--dropout', default=0.0, dest='dropout', type=float)
     parser.add_argument('--optimizer', default='AdamW', dest='Optim_NAME', type=str)
     parser.add_argument('--lr', default=0.0005, dest='lr_init', type=float)
     parser.add_argument('--lr_decay', default=1e-5, dest='lr_decay', type=float)
     parser.add_argument('--a_min', default=0.0, type=float, help='a_min in ScaleIntensityRanged')
     parser.add_argument('--a_max', default=2000.0, type=float, help='a_max in ScaleIntensityRanged')
+
+    # UNETR 전용
+    parser.add_argument('--embed', default=768, dest='embed_dim', type=int)
+    parser.add_argument('--mlp_dim', default=3072, dest='mlp_dim', type=int)
+    parser.add_argument('--num_layers', default=12, dest='num_layers', type=int)
+    parser.add_argument('--ext_layers', default='3,6,9,12', dest='ext_layers', type=str)
+    parser.add_argument('--input', default='96,96,96', dest='input_shape', type=str)
+
+    parser.add_argument('--patch', default=32, dest='patch_size', type=int)
+    parser.add_argument('--num_heads', default=12, dest='num_heads', type=int)
     args = parser.parse_args()
 
 
     os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
+    args.pth_path = os.path.join(args.model_path, args.save_name, 'model_best.pth')
 
-    print(args.output_dir)
-    print(args.MODEL_NAME)
+    print(args.pth_path)
+    print(args.save_name)
 
     roi_size = [args.roi_x, args.roi_y, args.roi_z]
+    args.input_shape = [int(this_) for this_ in args.input_shape.split(',')]
     
     args.use_cuda = torch.cuda.is_available()
     args.device = torch.device("cuda" if args.use_cuda else "cpu")
-    rst_dir = os.path.join(args.output_dir, args.MODEL_NAME)
+    rst_dir = os.path.join(args.output_dir, args.save_name)
     if os.path.isdir(rst_dir)== False:
         os.makedirs(rst_dir)
 
@@ -69,7 +80,6 @@ def main():
 
     test_org_transforms = Compose(
         [
-
             LoadImaged(keys="image"),
             EnsureChannelFirstd(keys="image"),
             ScaleIntensityRanged(keys=["image"], 
@@ -77,15 +87,6 @@ def main():
                 b_min=0, b_max=1, clip=True),
             CropForegroundd(keys=["image"], source_key="image"),
             EnsureTyped(keys="image"),
-
-            # LoadImaged(keys=["image", "label"]),
-            # EnsureChannelFirstd(keys=["image", "label"]),
-            # AsDiscreted(keys=["label"], to_onehot=args.channel_out),
-            # ScaleIntensityRanged(keys=["image"], 
-            #     a_min=args.a_min, a_max=args.a_max, 
-            #     b_min=0, b_max=1, clip=True),
-            # CropForegroundd(keys=["image", "label"], source_key="image"),
-            # ToTensord(keys=["image", "label"]),
         ]
     )
 
@@ -94,30 +95,9 @@ def main():
 
     test_org_loader = DataLoader(test_org_ds, batch_size=1, num_workers=4)
 
-    # post_transforms = Compose(
-    #     EnsureTyped(keys="pred"),
-    #     Invertd(
-    #         keys="pred",
-    #         transform=test_org_transforms,
-    #         orig_keys="image",
-    #         meta_keys="pred_meta_dict",
-    #         orig_meta_keys="image_meta_dict",
-    #         meta_key_postfix="meta_dict",
-    #         nearest_interp=False,
-    #         to_tensor=True,
-    #     ),
-    #     AsDiscreted(keys="pred", argmax=True, to_onehot=2),
-    #     SaveImaged(keys="pred", meta_keys="pred_meta_dict", output_dir=rst_dir, output_postfix="seg", resample=False),
-    
-    # post_trans = Compose([Activations(sigmoid=True), AsDiscrete(threshold=0.5)])
-
     post_trans = Compose([EnsureType(), Activations(sigmoid=True), AsDiscrete(threshold_values=0.5)])
 
     saver = SaveImage(output_dir=rst_dir, output_ext=".nii.gz", output_postfix="seg")
-    
-    # saver = SaveImage(keys='data', output_dir=rst_dir,  output_ext=".nii.gz", output_postfix="seg")
-    
-
 
 
     model = call_only_model(args)
@@ -125,7 +105,6 @@ def main():
 
 
     model.load_state_dict(torch.load(args.pth_path)["model_state_dict"])
-        # os.path.join(args.pth_path, "best_metric_model.pth")))
     model.eval()
 
 
@@ -151,14 +130,9 @@ def main():
         for test_data in test_org_loader:
             test_inputs = test_data["image"].to(device)
             sw_batch_size = 4
-            # test_name = test_data['image_meta_dict']['filename_or_obj'][0][-17:]
 
-            # test_output = activation(sliding_window_inference(test_inputs, inf_size, 4, model))
             pred_data = sliding_window_inference(test_inputs, roi_size, sw_batch_size, model)
             test_outputs = [post_trans(i) for i in decollate_batch(pred_data)]
-
-            # for test_output in test_outputs:
-            #     saver(test_output)
 
             meta_data = decollate_batch(test_data["image_meta_dict"])
             for test_output, data in zip(test_outputs, meta_data):
